9bc4e2cecb399bb478de81db83aa6c967e2f4013
ukozazenje <mzecevic04@gmail.com>
2019-05-08 18:54:38 +0200

    saving progress

--- src/routes/form/PageOne.js
+++ src/routes/form/PageOne.js
@@ -11,7 +11,7 @@ const onChange = (ctx, val) => console.log(`${val}% complete`);
 
 // TODO Make ZipSubmit component (onClick send to top of page)
 class PageOne extends Component  {
-
+	
 	getStateAndCity = async (zip) => {
 		let response =  fetch(`https://api.zippopotam.us/us/${zip}`, {
 			method: 'GET'
@@ -53,57 +53,57 @@ class PageOne extends Component  {
 	render(){
 		
 		return (
-			<div className="page">
+		<div className="page">
 			<main>
 				<div class="mast">
 					<h2>Find the right health insurance for you in Philadelphia, PA.</h2>
 					<div class="mast-image"></div>
 					<h3>Get Started!</h3>
 					<div class="input-group">
-								<label htmlFor="numberOnPolicy" name="numberOnPolicy" >Number on Policy</label>
-								<select htmlFor="numberOnPolicy" value={this.props.values.numberOnPolicy} name="numberOnPolicy" placeholder="1" onChange={(e) => { this.props.handleChange(e); this.props.handleLocalStorage(e) } }>
-									<option value="">Select</option>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-									<option value="4+">4+</option>
-								</select>
-								<label htmlFor="home_zip" name="home_zip">Zip Code</label>
-							<Field	
-									name="home_zip"
-									render={({ field }) => (
-										<MaskedInput
-											{...field}
-											mask={[/[0-9]/, /\d/, /\d/, /\d/, /\d/]}
-											placeholder="ZIP Code"
-											type="text"
-											guide={true}
-											onChange={(e)=>{
-												this.props.handleChange(e);
-												this.props.handleLocalStorage(e);
-											}}
-										/>
-									)}
-								/>
-								<button
-									type="button"
-									onClick={ this.handleNext }
-									disabled={!(this.checkForErrors(this.props.errors) && this.props.values.numberOnPolicy && this.props.values.home_zip)}
-								>
-									Next
-								</button>
-								<ErrorMessage
-									name="numberOnPolicy"
-									component="div"
-									className="field-error"
-								/>
-								<ErrorMessage
-									name="home_zip"
-									component="div"
-									className="field-error"
-								/>
+						<label htmlFor="numberOnPolicy" name="numberOnPolicy" >Number on Policy</label>
+						<select htmlFor="numberOnPolicy" value={this.props.values.numberOnPolicy} name="numberOnPolicy" placeholder="1" onChange={(e) => { this.props.handleChange(e); this.props.handleLocalStorage(e) } }>
+							<option value="">Select</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4+">4+</option>
+						</select>
+						<label htmlFor="home_zip" name="home_zip">Zip Code</label>
+						<Field	
+								name="home_zip"
+								render={({ field }) => (
+									<MaskedInput
+										{...field}
+										mask={[/[0-9]/, /\d/, /\d/, /\d/, /\d/]}
+										placeholder="ZIP Code"
+										type="text"
+										guide={true}
+										onChange={(e)=>{
+											this.props.handleChange(e);
+											this.props.handleLocalStorage(e);
+										}}
+									/>
+								)}
+							/>
+							<button
+								type="button"
+								onClick={ this.handleNext }
+								disabled={!(this.checkForErrors(this.props.errors) && this.props.values.numberOnPolicy && this.props.values.home_zip)}
+							>
+								Next
+							</button>
+							<ErrorMessage
+								name="numberOnPolicy"
+								component="div"
+								className="field-error"
+							/>
+							<ErrorMessage
+								name="home_zip"
+								component="div"
+								className="field-error"
+							/>
+						</div>
 					</div>
-				</div>
 				<div class="plans">
 					<nav>
 						<ul>
@@ -127,117 +127,117 @@ class PageOne extends Component  {
 					</nav>
 				</div>
 				<div class="about">
-				<h3>Everything you need for your health insurance choices in one place!</h3>
-				<div class="about-image"></div>
-				<div id="how-it-works" class="how-it-works">
-					<h4>Guided Online Experience</h4>
-					<p>We know not many people are healthcare experts, so we provide explanations, product guides, and advice along the way.</p>
-					<h4>Compare XXXXXX+ Insurance Plans and Save</h4>
-					<p>We work hard to deliver the best health plan shopping experience on the web. And it costs you nothing to use our search.</p>
-					<h4>Personalized Recommendations</h4>
-					<p>We sort through your options in our database, and point you in the right direction based on your needs, preferences, and budget.</p>
-					<div class="input-group">
-								<label htmlFor="numberOnPolicy" name="numberOnPolicy" >Number on Policy</label>
-								<select htmlFor="numberOnPolicy" value={this.props.values.numberOnPolicy} name="numberOnPolicy" placeholder="1" onChange={(e) => { this.props.handleChange(e); this.props.handleLocalStorage(e) } }>
-									<option value="">Select</option>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-									<option value="4+">4+</option>
-								</select>
-								<label htmlFor="home_zip" name="home_zip">Zip Code</label>
+					<h3>Everything you need for your health insurance choices in one place!</h3>
+					<div class="about-image"></div>
+					<div id="how-it-works" class="how-it-works">
+						<h4>Guided Online Experience</h4>
+						<p>We know not many people are healthcare experts, so we provide explanations, product guides, and advice along the way.</p>
+						<h4>Compare XXXXXX+ Insurance Plans and Save</h4>
+						<p>We work hard to deliver the best health plan shopping experience on the web. And it costs you nothing to use our search.</p>
+						<h4>Personalized Recommendations</h4>
+						<p>We sort through your options in our database, and point you in the right direction based on your needs, preferences, and budget.</p>
+						<div class="input-group">
+							<label htmlFor="numberOnPolicy" name="numberOnPolicy" >Number on Policy</label>
+							<select htmlFor="numberOnPolicy" value={this.props.values.numberOnPolicy} name="numberOnPolicy" placeholder="1" onChange={(e) => { this.props.handleChange(e); this.props.handleLocalStorage(e) } }>
+								<option value="">Select</option>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4+">4+</option>
+							</select>
+							<label htmlFor="home_zip" name="home_zip">Zip Code</label>
 							<Field
-												name="home_zip"
-												render={({ field }) => (
-													<MaskedInput
-														{...field}
-														mask={[/[0-9]/, /\d/, /\d/, /\d/, /\d/]}
-														placeholder="ZIP Code"
-														type="text"
-														guide={true}
-														onChange={(e)=>{
-															this.props.handleChange(e);
-															this.props.handleLocalStorage(e);
-														}}
-													/>
-												)}
-											/>
-								<button
-									type="button"
-									onClick={this.handleNext}
-									disabled={!(this.checkForErrors(this.props.errors) && this.props.values.numberOnPolicy && this.props.values.home_zip)}
-								>
-									Next
-								</button>
-								<ErrorMessage
-									name="numberOnPolicy"
-									component="div"
-									className="field-error"
-								/>
-								<ErrorMessage
-									name="home_zip"
-									component="div"
-									className="field-error"
-								/>
+								name="home_zip"
+								render={({ field }) => (
+									<MaskedInput
+										{...field}
+										mask={[/[0-9]/, /\d/, /\d/, /\d/, /\d/]}
+										placeholder="ZIP Code"
+										type="text"
+										guide={true}
+										onChange={(e)=>{
+											this.props.handleChange(e);
+											this.props.handleLocalStorage(e);
+										}}
+									/>
+								)}
+							/>
+							<button
+								type="button"
+								onClick={this.handleNext}
+								disabled={!(this.checkForErrors(this.props.errors) && this.props.values.numberOnPolicy && this.props.values.home_zip)}
+							>
+								Next
+							</button>
+							<ErrorMessage
+								name="numberOnPolicy"
+								component="div"
+								className="field-error"
+							/>
+							<ErrorMessage
+								name="home_zip"
+								component="div"
+								className="field-error"
+							/>
+						</div>
 					</div>
-					</div>
-					</div>
-					<div id="stats" class="stats">
-						<p>XXXXXXX+ People we’ve helped shop for insurance</p>
-						<p>XXXXX Insurance Plans Available</p>
-						<p>3 minutes to get a quote</p>
-					</div>
-					<div id="testimonials" class="testimonials">
-						<h3>We’ve helped XXXXX of Americans shop for Helathcare</h3>
-						<p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo neque, feugiat sit amet lacus in, dapibus convallis ligula. Vivamus ornare sed ligula sed porta.”<br/><br/>- Some Person</p>
-						<p>“In hac habitasse platea dictumst. Mauris laoreet massa et nibh dapibus bibendum. Nunc mauris nulla, tincidunt in tristique.”<br/><br/>- Some Person</p>
+				</div>
+				<div id="stats" class="stats">
+					<p>XXXXXXX+ People we’ve helped shop for insurance</p>
+					<p>XXXXX Insurance Plans Available</p>
+					<p>3 minutes to get a quote</p>
+				</div>
+				<div id="testimonials" class="testimonials">
+					<h3>We’ve helped XXXXX of Americans shop for Helathcare</h3>
+					<p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo neque, feugiat sit amet lacus in, dapibus convallis ligula. Vivamus ornare sed ligula sed porta.”<br/><br/>- Some Person</p>
+					<p>“In hac habitasse platea dictumst. Mauris laoreet massa et nibh dapibus bibendum. Nunc mauris nulla, tincidunt in tristique.”<br/><br/>- Some Person</p>
 
-								<div class="input-group">
-								<label htmlFor="numberOnPolicy" name="numberOnPolicy" >Number on Policy</label>
-								<select htmlFor="numberOnPolicy" value={this.props.values.numberOnPolicy} name="numberOnPolicy" placeholder="1" onChange={(e) => {this.props.handleChange } }>
-									<option value="">Select</option>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-									<option value="4+">4+</option>
-								</select>
-								<label htmlFor="home_zip" name="home_zip">Zip Code</label>
-							<Field
-												name="home_zip"
-												render={({ field }) => (
-													<MaskedInput
-														{...field}
-														mask={[/[0-9]/, /\d/, /\d/, /\d/, /\d/]}
-														placeholder="ZIP Code"
-														type="text"
-														guide={true}
-														onChange={(e)=>{
-															this.props.handleChange(e);
-															this.props.handleLocalStorage(e);
-														}}
-													/>
-												)}
-											/>
-								<button
-									type="button"
-									onClick={this.handleNext}
-									disabled={!(this.checkForErrors(this.props.errors) && this.props.values.numberOnPolicy && this.props.values.home_zip)}
-								>
-									Next
-								</button>
-								<ErrorMessage
-									name="numberOnPolicy"
-									component="div"
-									className="field-error"
+					<div class="input-group">
+						<label htmlFor="numberOnPolicy" name="numberOnPolicy" >Number on Policy</label>
+						<select htmlFor="numberOnPolicy" value={this.props.values.numberOnPolicy} name="numberOnPolicy" placeholder="1" onChange={(e) => { this.props.handleChange(e); this.props.handleLocalStorage(e) } }>
+							<option value="">Select</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4+">4+</option>
+						</select>
+						<label htmlFor="home_zip" name="home_zip">Zip Code</label>
+						<Field
+							name="home_zip"
+							render={({ field }) => (
+								<MaskedInput
+									{...field}
+									mask={[/[0-9]/, /\d/, /\d/, /\d/, /\d/]}
+									placeholder="ZIP Code"
+									type="text"
+									guide={true}
+									onChange={(e)=>{
+										this.props.handleChange(e);
+										this.props.handleLocalStorage(e);
+									}}
 								/>
-								<ErrorMessage
-									name="home_zip"
-									component="div"
-									className="field-error"
-								/>
+							)}
+						/>
+						<button
+							type="button"
+							onClick={this.handleNext}
+							disabled={!(this.checkForErrors(this.props.errors) && this.props.values.numberOnPolicy && this.props.values.home_zip)}
+						>
+							Next
+						</button>
+						<ErrorMessage
+							name="numberOnPolicy"
+							component="div"
+							className="field-error"
+						/>
+						<ErrorMessage
+							name="home_zip"
+							component="div"
+							className="field-error"
+						/>
 					</div>
-					</div>
-				</main>
+				</div>
+			</main>
 		</div>
 	);
 }

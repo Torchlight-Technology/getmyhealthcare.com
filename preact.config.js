5170a6bd755203c3fdaebabf8a76590dc2ee2bd2
Slobodan Perichin <perichin@gmail.com>
2019-05-11 00:03:40 +0200

    added env file and logic

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,20 @@
 import asyncPlugin from 'preact-cli-plugin-async';
 
-export default (config) => {
+export default function (config, env, helpers) {
+
+    console.log('isProd: ', env.isProd);
+
+    if (!env.isProd) {
+        require('dotenv').config({
+            path: '.env.development'
+        })
+    }
+
+    config.plugins.push(
+        new helpers.webpack.DefinePlugin({
+            'process.env.API_URL': JSON.stringify(process.env.API_URL)
+        })
+    );
+
     asyncPlugin(config);
 }
